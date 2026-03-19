c6ab56cf40cd4ec634a72611ebf93f822bdc7566
Jimmy Piedrahita <jimmy22piedrahita@gmail.com>
2026-03-18 21:15:31 -0500

    se agrega una ventana con tkinter

---
+++ interfaz.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Asistente de Compras")
+    root.geometry("400x300")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,4 @@
+﻿import interfaz
+
+if __name__ == "__main__":
+    interfaz.main()
